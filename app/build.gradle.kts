81f8981d6c74f04484f6805567afb7f3b14153e1
NIKHIL <nikhilyadavsky2004@gmail.com>
2026-05-08 19:46:55 +0530

    Fix APK signature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
